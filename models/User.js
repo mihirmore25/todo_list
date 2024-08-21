ae16a3b71342250b569deadd83873f80cdb7aae6
Mihir More <mihirmore.25@gmail.com>
2024-08-21 17:47:54 +0530

    Add Encrypt/Hash user password with user creation

--- models/User.js
+++ models/User.js
@@ -54,4 +54,21 @@ userSchema.methods.generateJWT = function () {
     });
 };
 
+userSchema.pre("save", function (next) {
+    const user = this;
+
+    if (!user.isModified("password")) return next();
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
 export const User = mongoose.model("user", userSchema);
